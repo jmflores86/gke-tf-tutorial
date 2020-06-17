ecd515d8974e06c551af22b2688ac93067c71c06
jmflores86 <43918661+jmflores86@users.noreply.github.com>
2020-06-17 12:10:39 -0500

    changed credentials file name

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("./creds/serviceaccount.json")
+  credentials = file("./creds/terraform.json")
   project     = "or2-m-epm-gcp-us-anthos-t1iylu"
   region      = "europe-west1"
 }
