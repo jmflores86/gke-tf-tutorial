f571d301ea6c2b7fc33d57d92686b7d234e3a2bb
jmflores86 <43918661+jmflores86@users.noreply.github.com>
2020-06-17 12:11:33 -0500

    sadf

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "my-first-gke-cluster-jenkins"
+  name               = "my-first-gke-cluster-2"
   network            = "default"
   location           = "europe-west1-b"
   initial_node_count = 3
