586948102c0eb8c9d16ba6f062befb8640df7e43
jmflores86 <43918661+jmflores86@users.noreply.github.com>
2020-06-17 12:08:23 -0500

    awer

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,11 +1,3 @@
-data "terraform_remote_state" "prod-tfstate" {
-  backend = "gcs"
-  config = {
-    bucket  = "or2-m-epm-gcp-us-anthos-t1iylu-tfstate"
-    prefix  = "prod"
-  }
-}
-
 resource "google_container_cluster" "gke-cluster" {
   name               = "my-first-gke-cluster-jenkins"
   network            = "default"
